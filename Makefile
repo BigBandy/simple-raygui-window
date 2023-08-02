fc85aa469814e24d9254c76feb6cdbf0cbd7daae
Andy <51020735+BigBandy@users.noreply.github.com>
2023-08-02 03:28:23 -0700

    Initial commit

---
+++ Makefile
@@ -0,0 +1,4 @@
+# NOTE: For Windows only. Will require tinkering for other platforms. If unsure, use raylib makefile homie.
+
+default:
+	gcc ./src/*.c -o gui_test.exe -fdiagnostics-color=always -g -I C:\Users\Andy\source\repos\raylib\src -L C:\Users\Andy\source\repos\raylib\src -static -lraylib -lopengl32 -lgdi32 -lwinmm
---
+++ src/main.c
@@ -0,0 +1,106 @@
+#include <raylib.h>
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+const int screen_width = 640;
+const int screen_height = 480;
+
+Rectangle windowBounds = {
+    0.0f, 0.0f,
+    120.0f, 240.0f
+};
+bool windowActive = true;
+bool windowGrabbed = false;
+bool windowStretching = false;
+const char *windowTitle = "raygui";
+
+Rectangle windowOpenBounds = {
+    0.0f, 0.0f,
+    24.0f, 24.0f
+};
+const char *windowOpenText = "#214#";
+
+int main(void) {
+    InitWindow(screen_width, screen_height, windowTitle);
+    SetExitKey(KEY_NULL);
+
+    GuiLoadStyle("resources/dark.rgs");
+
+    while (!WindowShouldClose()) {
+        const Vector2 mouse_position = GetMousePosition();
+        const Vector2 mouse_delta = GetMouseDelta();
+
+        const bool m1 = IsMouseButtonDown(MOUSE_BUTTON_LEFT);   // Is left mouse down?
+        
+        // Update
+
+        if (windowActive) {
+            // Window dragging logic
+            if ((CheckCollisionPointRec(mouse_position, (Rectangle){windowBounds.x, windowBounds.y, windowBounds.width, 24}) || windowGrabbed) && m1) {
+                windowGrabbed = true;
+
+                windowBounds.x += mouse_delta.x;
+                windowBounds.y += mouse_delta.y;
+            } else {
+                windowGrabbed = false;
+
+                if (windowBounds.x < 0) {
+                    windowBounds.x = 0;
+                } else if (windowBounds.x > (float)screen_width - windowBounds.width) {
+                    windowBounds.x = (float)screen_width - windowBounds.width;
+                }
+
+                if (windowBounds.y < 0) {
+                    windowBounds.y = 0;
+                } else if (windowBounds.y > (float)screen_height - windowBounds.height) {
+                    windowBounds.y = (float)screen_height - windowBounds.height;
+                }
+
+
+            }
+        }
+        
+        // Draw (and a little updating bc immediate mode, y'know)
+        
+        BeginDrawing();
+            
+            ClearBackground(BLACK);
+
+            if (windowActive) {
+                windowActive = !GuiWindowBox(windowBounds, windowTitle);
+
+                // Window stretching logic
+                if ((CheckCollisionPointRec(mouse_position, (Rectangle){windowBounds.x + windowBounds.width - 4, windowBounds.y + windowBounds.height - 4, 8, 8}) || windowStretching)) { // <- rectangle is bottom right corner of window
+                    SetMouseCursor(MOUSE_CURSOR_RESIZE_NWSE);
+                    
+                    if (m1) {
+                        windowStretching = true;
+
+                        windowBounds.width += mouse_delta.x;
+                        windowBounds.height += mouse_delta.y;
+
+                        if (windowBounds.width < MeasureTextEx(GuiGetFont(), windowTitle, (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING)).x + 48) {
+                            windowBounds.width = MeasureTextEx(GuiGetFont(), windowTitle, (float)GuiGetStyle(DEFAULT, TEXT_SIZE), (float)GuiGetStyle(DEFAULT, TEXT_SPACING)).x + 48;
+                        } else if (windowBounds.width > screen_width - windowBounds.x) {
+                            windowBounds.width = screen_width - windowBounds.x;
+                        }
+
+                        if (windowBounds.height > screen_height - windowBounds.y) {
+                            windowBounds.height = screen_height - windowBounds.y;
+                        }
+                    } else windowStretching = false;
+                    
+                } else {
+                    SetMouseCursor(MOUSE_CURSOR_ARROW);
+                    windowStretching = false;
+                }
+            } else windowActive = GuiButton(windowOpenBounds, windowOpenText);
+        
+        EndDrawing();
+    }
+
+    CloseWindow();
+
+    return 0;
+}
